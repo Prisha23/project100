f872e2ef38dfade7c78354eaaa5a3adc048bebc2
Prisha23 <68731838+Prisha23@users.noreply.github.com>
2022-06-01 22:21:52 +0530

    Add files via upload

---
+++ Project100.py
@@ -0,0 +1,10 @@
+class atm(object):
+    def __init__(self,cashwithdrawl,balanceenquiry):
+        self.cashwithdrawl=cashwithdrawl
+        self.balanceenquiry=balanceenquiry
+
+    def cashwithdrawl(self):
+        print("started")
+
+    def balanceenquiry(self):
+        print("stop")
